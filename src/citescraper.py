eadea57164290c0f60d47b2b622b489e0f5731f8
Nathan Self <nwself@cs.vt.edu>
2017-12-15 12:08:35 -0500

    Use commandline for DOI

--- src/citescraper.py
+++ src/citescraper.py
@@ -1,29 +1,19 @@
+import argparse
 import requests
 from bs4 import BeautifulSoup
 import json
+from urlparse import urlparse, parse_qs
 
 
-def get_citations(doi):
-    url = "http://citeseerx.ist.psu.edu/viewdoc/citations"
-    payload = {'doi': doi}
-
-    response = requests.get(url, params=payload)
-
-    if response.status_code != 200:
-        print('ERROR code {} for {}'.format(response.status_code, doi))
-
-    soup = BeautifulSoup(response.content, "html.parser")
-
-    citations = []
-
-    for tr in soup.find(id="citations").table.find_all('tr'):
+class Citation():
+    def __init__(self, tr):
+        """Take a BeautifulSoup Tag for a tr in the citation list table."""
         td = tr.find_all('td')[1]
         attrs = td.a.attrs
-        citation = {
-            'title': td.a.string,
-            'url': attrs['href'],
-            'citation_only': 'class' in attrs and 'citation_only' in attrs['class'],
-        }
+
+        self.title = td.a.string
+        self.url = attrs['href']
+        self.citation_only = 'class' in attrs and 'citation_only' in attrs['class']
 
         # hoping that contents[2] will always be the author/year string
         author_year = td.contents[2].string.split() if len(td.contents) > 2 else []
@@ -32,56 +22,100 @@ def get_citations(doi):
         if author_year and author_year[0] == '-':
             next_dash = author_year.index('-', 1) if '-' in author_year[1:] else len(author_year)
 
-            citation['author'] = ' '.join(author_year[1:next_dash])
-            if next_dash + 1 < len(author_year):
-                citation['year'] = author_year[next_dash + 1]
+            self.author = ' '.join(author_year[1:next_dash])
+            self.year = author_year[next_dash + 1] if next_dash + 1 < len(author_year) else None
 
         context = td.select('p.citationContext')
         if context:
-            citation['context'] = context[0].string.strip()
+            self.context = context[0].string.strip()
 
-        citations.append(citation)
+    def follow(self):
+        if not self.citation_only:
+            cid = cid_from_url(self.url)
+            return Record(cid, identifier_key="cid")
 
-    return citations
+
+def doi_from_url(url):
+    qs = parse_qs(urlparse(url).query)
+    return qs['doi'][0] if 'doi' in qs and qs['doi'] else None
 
 
-def get_record(doi):
-    url = "http://citeseerx.ist.psu.edu/viewdoc/versions"
-    payload = {'doi': doi}
+def cid_from_url(url):
+    qs = parse_qs(urlparse(url).query)
+    return qs['cid'][0] if 'cid' in qs and qs['cid'] else None
 
-    response = requests.get(url, params=payload)
 
-    if response.status_code != 200:
-        print('ERROR code {} for {}'.format(response.status_code, doi))
+class Record():
+    def __init__(self, identifier, identifier_key="doi"):
+        """Fetch a record.
 
-    soup = BeautifulSoup(response.content, "html.parser")
+        identifier_key can be "doi" or "cid"
+        identifier is the actual doi or cid identification number
+        """
+        url = "http://citeseerx.ist.psu.edu/viewdoc/versions"
+        payload = {identifier_key: identifier}
 
-    record = {
-        'doi': doi,
-        'pdf_url': "http://citeseerx.ist.psu.edu/viewdoc/download?doi={}&rep=rep1&type=pdf".format(doi),
-        'authors': [],
-    }
+        response = requests.get(url, params=payload)
 
-    for tr in soup.find_all('tr'):
-        if tr.td:
-            datum = tr.td.string.lower()
+        if response.status_code != 200:
+            print('ERROR code {} for {}'.format(response.status_code, doi))
 
-            if datum == "title":
-                record['title'] = tr.find_all('td')[1].string
-            elif datum == "abstract":
-                record['abstract'] = tr.find_all('td')[1].string
-            elif datum == "year":
-                record['year'] = tr.find_all('td')[1].string
-            elif datum == "venue":
-                record['venue'] = tr.find_all('td')[1].string
-            elif datum == "author name":
-                record['authors'].append(tr.find_all('td')[1].string)
+        soup = BeautifulSoup(response.content, "html.parser")
 
-    record['citations'] = get_citations(doi)
+        self.doi = doi_from_url(response.url)
+        self.pdf_url = "http://citeseerx.ist.psu.edu/viewdoc/download?doi={}&rep=rep1&type=pdf".format(self.doi)
+        self.authors = []
 
-    print(json.dumps(record))
+        for tr in soup.find_all('tr'):
+            if tr.td:
+                datum = tr.td.string.lower()
+
+                if datum == "title":
+                    self.title = tr.find_all('td')[1].string
+                elif datum == "abstract":
+                    self.abstract = tr.find_all('td')[1].string
+                elif datum == "year":
+                    self.year = tr.find_all('td')[1].string
+                elif datum == "venue":
+                    self.venue = tr.find_all('td')[1].string
+                elif datum == "author name":
+                    self.authors.append(tr.find_all('td')[1].string)
+
+        self.citations = self.get_citations(self.doi)
+
+    def get_citations(self, doi):
+        url = "http://citeseerx.ist.psu.edu/viewdoc/citations"
+        payload = {'doi': doi}
+
+        response = requests.get(url, params=payload)
+
+        if response.status_code != 200:
+            print('ERROR code {} for {}'.format(response.status_code, doi))
+
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        citations = []
+
+        for tr in soup.find(id="citations").table.find_all('tr'):
+            c = Citation(tr)
+            citations.append(c)
+
+        return citations
+
+    def toJSON(self):
+        obj = self.__dict__
+        obj['citations'] = [c.__dict__ for c in obj['citations']]
+        return obj
 
 
 if __name__ == "__main__":
-    get_record('10.1.1.874.7127')
-    # get_record('10.1.1.550.47')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("doi", help="A DOI for a paper on CiteSeerX (e.g. 10.1.1.874.7127)")
+    args = parser.parse_args ()
+
+    record = Record(args.doi)
+
+    # record = Record('10.1.1.874.7127')
+    # record = get_record('10.1.1.550.47')
+    # record = get_record('10.1.1.10.3979')
+    print(json.dumps(record.toJSON()))
